5e4ad40561d82649085f39a07c6041f3aaec2134
atif2843 <muhammadatif93@gmail.com>
2023-03-01 15:28:54 +0530

    Changed comming soon page to live web page

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let cross = document.querySelector(".navbar-toggler");
+
+cross.addEventListener("click", () => {
+    cross.classList.toggle("cross");
+});
+
--- demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-$txt = $_POST['a']."\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
